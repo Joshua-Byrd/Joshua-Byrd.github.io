865eaa22083c8bffd7b8f8403816717596661074
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-17 10:35:18 -0500

    Add message validation to contact page.

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,19 +2,37 @@
 
 window.onload = function()
 {
-   
-    let message = document.querySelector("#message");
+    const message = document.querySelector("#message");
     message.addEventListener("focus", eraseText);
     message.addEventListener("blur", printWarning);
 
+    const submitButton = document.querySelector("input[name='submit']");
+    submitButton.addEventListener("click", validateMessage);
 }
 
+/*
+Removes the message text when a user clicks in the textarea.
+*/
 function eraseText() {
     message.innerHTML = ""; 
 }
 
+/*
+Prints warning if user moves away from message and it is still blank
+*/
 function printWarning() {
     if (message.innerHTML === "") {
         document.querySelector("#warning-box").innerHTML = "Message cannot be blank!";
     }
 }
+
+/*
+Prevents user from sending a blank messsage and prints warning
+*/
+function validateMessage() {
+    if (message.innerHTML === "") {
+        printWarning();
+    }
+
+}
+
