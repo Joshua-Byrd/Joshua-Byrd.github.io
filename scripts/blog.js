1667d6c635f7d27c4323a6a33c0f985cd1f862aa
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-18 09:28:09 -0500

    Add randomized div blog post

---
+++ scripts/blog.js
@@ -0,0 +1,38 @@
+
+//Select the blog div and call the randomization functions on click
+const blogPostDiv1118 = document.querySelector('#blog-post-div-11-18-23');
+blogPostDiv1118.addEventListener('click', () => {
+    blogPostDiv1118.style.color = selectRandomColor();
+    blogPostDiv1118.style.backgroundColor = selectRandomColor();
+    blogPostDiv1118.style.border = selectRandomBorder();
+})
+
+/*
+Constructs and returns as a string a random color by randomizing the RGB values.
+*/
+function selectRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+/*
+Constructs and returns as a string a border style by randomizing the size (up to 10 pixels), the style, and the color.
+*/
+function selectRandomBorder() {
+    const borderStyle = ['solid', 'dotted', 'dashed', 'double', 'groove', 'ridge', 'inset', 'outset'];
+
+    //select random pixel size < 10
+    const px = Math.floor(Math.random() * 10) + 'px';
+
+    //select random style
+    const style = borderStyle[Math.floor(Math.random() * borderStyle.length)]
+
+    //select random color
+    const borderColor = selectRandomColor();
+    
+    return `${px} ${style} ${borderColor}`;
+}
+ 
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,38 +1,8 @@
-/* Contact page---------------------------------------------------------------*/
-
-window.onload = function()
-{
-    const message = document.querySelector("#message");
-    message.addEventListener("focus", eraseText);
-    message.addEventListener("blur", printWarning);
-
-    const submitButton = document.querySelector("input[name='submit']");
-    submitButton.addEventListener("click", validateMessage);
-}
-
 /*
-Removes the message text when a user clicks in the textarea.
+Erase default text when user clicks in textarea
 */
-function eraseText() {
-    message.innerHTML = ""; 
-}
-
-/*
-Prints warning if user moves away from message and it is still blank
-*/
-function printWarning() {
-    if (message.innerHTML === "") {
-        document.querySelector("#warning-box").innerHTML = "Message cannot be blank!";
-    }
-}
-
-/*
-Prevents user from sending a blank messsage and prints warning
-*/
-function validateMessage() {
-    if (message.innerHTML === "") {
-        printWarning();
-    }
-
-}
+const message = document.querySelector("#message");
+message.addEventListener("focus", () => {
+    message.innerHTML = '';
+});
 
