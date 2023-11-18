f1d043f3be60d95c6a3b5a3f502027efac6a5d14
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-18 12:12:25 -0500

    Fix form validation - again

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,18 +3,25 @@ Erase default text when user clicks in textarea. This only happens once, so a us
 */
 const message = document.querySelector("#message");
 const warningBox = document.querySelector("#warning-box");
+const submitButton = document.querySelector("input[type='submit']");
 
-message.addEventListener('focus', () => {
-    message.value = '';
+message.addEventListener("focus", () => {
+    message.value = "";
 }, {once:true});
 
-message.addEventListener('focus', () => {
-    warningBox.innerHTML = '';
+message.addEventListener("focus", () => {
+    warningBox.innerHTML = "";
 });
 
-message.addEventListener('blur', () => {
-    if (message.value === '') {
-        warningBox.innerHTML = 'Message must not be blank.';
+message.addEventListener("blur", () => {
+    if (message.value === "") {
+        warningBox.innerHTML = "Message must not be blank.";
     }
 });
 
+submitButton.addEventListener("submit", (e)=> {
+    if (message.value === "") {
+        e.preventDefault;
+        warningBox.innerHTML = "Message must not be blank.";
+    }
+})
