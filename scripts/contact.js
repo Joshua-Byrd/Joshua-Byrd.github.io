f85ca42d9bbfcb3137ef9729f1a4f8b730d716a3
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-18 11:58:50 -0500

    Fix contact form validation

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,8 +1,20 @@
 /*
-Erase default text when user clicks in textarea
+Erase default text when user clicks in textarea. This only happens once, so a user can click away and come back to the message. If the user clicks away and the textarea is blank, a warning message appears. The message will disappear if the user clicks back into the textarea.
 */
 const message = document.querySelector("#message");
-message.addEventListener("focus", () => {
-    message.innerHTML = '';
+const warningBox = document.querySelector("#warning-box");
+
+message.addEventListener('focus', () => {
+    message.value = '';
+}, {once:true});
+
+message.addEventListener('focus', () => {
+    warningBox.innerHTML = '';
+});
+
+message.addEventListener('blur', () => {
+    if (message.value === '') {
+        warningBox.innerHTML = 'Message must not be blank.';
+    }
 });
 
