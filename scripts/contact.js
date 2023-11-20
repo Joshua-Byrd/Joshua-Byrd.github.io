651580f17dea6004ddcc4133691299af46fc897b
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-20 05:35:31 -0500

    Add experience to resume page

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,8 +1,8 @@
 /*
-Erase default text when user clicks in textarea. This only happens once, so a user can click away and come back to the message. If the user clicks away and the textarea is blank, a warning message appears. The message will disappear if the user clicks back into the textarea.
+Erase default text when user clicks in textarea. This only happens once, so a user can click away and come back to the message. If the user clicks away and the textarea is blank, a warning message appears. The message will disappear if the user clicks back into the textarea. Also prevent submission if empty.
 */
 const message = document.querySelector("#message");
-const warningBox = document.querySelector("#warning-box");
+const messageWarningBox = document.querySelector("#message-warning-box");
 const submitButton = document.querySelector("input[type='submit']");
 
 message.addEventListener("focus", () => {
@@ -10,18 +10,51 @@ message.addEventListener("focus", () => {
 }, {once:true});
 
 message.addEventListener("focus", () => {
-    warningBox.innerHTML = "";
+    messageWarningBox.innerHTML = "";
 });
 
 message.addEventListener("blur", () => {
     if (message.value === "") {
-        warningBox.innerHTML = "Message must not be blank.";
+        displayMessageWarning();
     }
 });
 
 submitButton.addEventListener("submit", (e)=> {
     if (message.value === "") {
         e.preventDefault;
-        warningBox.innerHTML = "Message must not be blank.";
+        displayMessageWarning();
     }
+})
+
+function displayMessageWarning() {
+    messageWarningBox.innerHTML = "Message must not be blank";
+}
+
+
+/*
+Check to make sure email contains the @ symbol and displays a warning if not.
+If the user clicks back into the email box, the warning disappears. Prevent submission if not the correct format.
+*/
+const email = document.querySelector("#email");
+const emailWarningBox = document.querySelector("#email-warning-box");
+
+email.addEventListener("blur", () => {
+    if (!email.value.includes("@")) {
+        displayEmailWarning();
+    }  
+})
+
+email.addEventListener("focus", ()=> {
+    emailWarningBox.innerHTML = "";
+})
+
+function displayEmailWarning() {
+    emailWarningBox.innerHTML = "Email address must contain '@'";
+}
+
+submitButton.addEventListener("submit", (e)=> {
+    if (!email.value.includes("@")) {
+        e.preventDefault;
+        displayEmailWarning();
+    }  
 })
