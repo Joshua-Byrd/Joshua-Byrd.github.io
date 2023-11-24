53d2fa3d33baae9d1e0fc0583cb19a3b3620d933
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-24 09:39:43 -0500

    Add test-index.html, scroll reveal experiment

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-const app = Vue.createApp({
-    data: function() {
-        return {
-            greeting: "Hello Vue JS!"
+window.addEventListener("scroll", reveal)
+
+function reveal() {
+    const hiddenSections = document.querySelectorAll(".hidden");
+
+    for (hidden of hiddenSections) {
+        const windowHeight = window.innerHeight;
+        let hiddenTop = hidden.getBoundingClientRect().top;
+        let hiddenPoint = 150;
+
+        if (hiddenTop - windowHeight - hiddenTop) {
+            hidden.classList.add("active");
+            hiddenSections[0].childNodes[3].classList.add("active");
+        } else {
+            hidden.classList.remove("active");
+            hiddenSections[0].childNodes[3].classList.remove("active");
         }
     }
-})
+}
 
-app.mount("#app");
+
+// const app = Vue.createApp({
+//     data: function() {
+//         return {
+//             greeting: "Hello Vue JS!"
+//         }
+//     }
+// })
+
+// app.mount("#app");
