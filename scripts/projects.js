88ab6c29b034231e444f6b56758ea513608d19ad
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-28 10:44:33 -0500

    Start creating new projects page with Vue, projects-test.html

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,11 +1,43 @@
 //Create and mount the app inside the main section of the page
 const app = Vue.createApp({
-    //data is a function that returns an object containing variables
+   
+    data: function() {
+        //data is a function that returns an object containing variables
+        return {}
+    },
+
+    methods: {}
+
+})
+//custom component representing a project
+app.component("project", {
+    template: `
+        <article class="project">
+            <header>
+                <h2>
+                    {{heading}}
+                </h2>
+                <h3 class="project-github-link"><a
+                        href="{{githubLink}}">See
+                        it on Github</a>
+                </h3>
+            </header>
+            <p class="project-description">{{projectDesc}}</p>
+        </article>
+        <footer><hr></footer>
+    `,
+
     data: function() {
         return {
-            greeting: "Hello!"
+        heading: "Test",
+        githubLink: "https://github.com/",
+        projectDesc: "lorem ipsum"
         }
+    },
+
+    methods: {
+        
     }
-}); 
+})
 app.mount("#vue-projects-main");
 
