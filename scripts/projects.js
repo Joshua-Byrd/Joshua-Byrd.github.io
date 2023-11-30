24266bf99f4417ee42aac259a504ab7c8e7a2614
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2023-11-30 10:15:09 -0500

    Got the projects page working in Vue FINALLY

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,46 +1,42 @@
+let projectCount = 0;
+
 //Create and mount the app inside the main section of the page
 const app = Vue.createApp({
    
     data: function() {
         //data is a function that returns an object containing variables
-        return {}
-    },
-
-    methods: {}
-
-})
-//custom component representing a project
-app.component("project", {
-    template: `
-        <article class="project">
-            <header>
-                <h2>
-                    {{heading}}
-                </h2>
-                <h3 class="project-github-link"><a
-                        href="{{githubLink}}">See
-                        it on Github</a>
-                </h3>
-            </header>
-            <p class="project-description">{{projectDesc}}</p>
-        </article>
-        <footer><hr></footer>
-    `,
-
-    data: function() {
         return {
-        heading: "Test",
-        githubLink: "https://github.com/",
-        projectDesc: "lorem ipsum"
+            projects: [
+                {
+                id: projectCount++,
+                heading: "Database for Fitness/Nutrition App",
+                github: "https://github.com/Joshua-Byrd/CS669_term_project",
+                description: "This PostgreSQL database was my term project for CS669 at Boston University. I built it with the idea that it could be used for a fitness and nutrition tracking app. With this database, an app could store data about users' workouts, food eaten, and so on, and query for statistics for both. Included in  the script are queries to populate all tables, stored procedures to apply transactions like creating a new template entry and associating a template with a users, and triggers for updating a users' personal bests and logging a password change by a user."},
+                {
+                id: projectCount++,
+                heading: "MVC App in Java",
+                github: "https://github.com/Joshua-Byrd/GroupProject",
+                description: "This was a group project, completed with another student, as the final project of CIT594 at The University of Pennsylvania, It utilizes a rudimentary MVC pattern where the model accepts either a JSON or CSV file of tabular data and parses it into various data structures, while the view provides the user with a CLI menu consisting of multiple data analysis functions. The controller of course acts as the go-between for the model and view and provides validation for incoming files before passing them to the model."},
+                {
+                id: projectCount++,
+                heading: "Data Analysis using Python and Jupyter Notebook",
+                github: "https://github.com/Joshua-Byrd/Exit_Surveys_Analysis/blob/master/Exit_Surveys_Analysis.ipynb",
+                description: " In this project, I analyzed two datasets using Python and Pandas in Jupyter Notebook. Both datasets consisted of employee exit surveys and were obtained from the Department of Education, Training, and Employment; and from the Technical and Further Education Institute, both in Queensland, Autralia. Each contained various data points about employees such as employee id and start and end dates, as well as information about why employees left their positions. The project consisted of cleaning the data to deal with null values and odd entries, as well as getting both sets into workable shape. Then basic analysis to determine why employees left their positions, if it differed between short- and long-term employees, and how much of a role job dissatisfaction played in their leaving."},
+                {
+                id: projectCount++,
+                heading: "Pool Maintenance Program",
+                github: "https://github.com/Joshua-Byrd/PMP",
+                description: "A simple CLI Python app for pool operators, PMP prompts the user for their chemical test readings,then calculates and prints the water's saturation index and what (if any) chemicals and how much to be added. Also provides options to export daily results to a csv file."}
+
+            ]
         }
     },
 
-    methods: {
-
-    }
+    methods: {},
 })
-//custom component for main navbar
-app.component("navbar", {
+
+//custom component for main header/navbar
+app.component("header-navbar", {
     template: `
         <header>
             <nav class="header-navbar">
@@ -66,9 +62,10 @@ app.component("navbar", {
             </h1>
         </header>
     `,
-    data: function() {
-        return {
-            header: "Projects"
+    props: {
+        header: {
+            type: String,
+            default: "DEFAULT HEADER"
         }
     },
     methods: {}
